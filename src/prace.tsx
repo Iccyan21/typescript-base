05c78f5d32311a2e2e4ead169b3d5b32c9266a56
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-06-22 17:28:03 +0900

    Third Commit

--- src/prace.tsx
+++ src/prace.tsx
@@ -5,14 +5,15 @@ interface CandidateData {
   PraceID: number;
   name: string;
   address: string;
-  title:string;
-  content:string;
+  title: string;
+  content: string;
+  image: string; // 修正: 画像のパスを保持するために string 型を使用
 }
 
 function YourComponent(props: { PraceID: number }) {
   const { PraceID } = props;
 
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<CandidateData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +21,7 @@ function YourComponent(props: { PraceID: number }) {
         const response = await fetch(`http://127.0.0.1:8000/post/posts/api/${PraceID}/`);
         const jsonData = await response.json();
         setData(jsonData);
+
       } catch (error) {
         console.error(error);
       }
@@ -32,6 +34,7 @@ function YourComponent(props: { PraceID: number }) {
     <div>
       {data.map((item) => (
         <div key={item.PraceID}>
+          <h1>{item.name}</h1>
           <h1>{item.title}</h1>
           <p>{item.content}</p>
         </div>
@@ -44,6 +47,7 @@ function CandidateDataComponent() {
   const [data, setData] = useState<CandidateData | null>(null);
   const { name }: { name?: string } = useParams();
 
+  
   useEffect(() => {
     const fetchData = async () => {
       if (name) {
@@ -52,6 +56,8 @@ function CandidateDataComponent() {
           const response = await fetch(`http://127.0.0.1:8000/gmap/parace/${encodedName}/`);
           const jsonData: CandidateData = await response.json();
           setData(jsonData);
+          console.log(jsonData)
+          console.log(jsonData.image)
         } catch (error) {
           console.error(error);
         }
@@ -60,13 +66,15 @@ function CandidateDataComponent() {
 
     fetchData();
   }, [name]);
-
+  
   return (
     <div>
       {data !== null ? (
         <div>
           <h1>{data.name}</h1>
           <p>{data.address}</p>
+          {/* Image */}
+          <img src={`http://127.0.0.1:8000${data.image}`} alt={data.name} width="380" height="230"/>
         </div>
       ) : (
         <div>Loading...</div>
